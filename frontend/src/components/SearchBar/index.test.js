419f6a104f6a6a51b5ed97b4aa35c29086b3410c
Siu Kei Tam <tam.s.k.92@gmail.com>
2022-05-16 12:18:40 +0100

    updated tests

--- frontend/src/components/SearchBar/index.test.js
+++ frontend/src/components/SearchBar/index.test.js
@@ -1,5 +1,23 @@
-//Test that the component renders properly and matches snapshot
+import { render } from '@testing-library/react'
+import { SearchBar } from './' 
 
-//Test to check that searchString state updates when the user is typing
 
-//Test that pressing the enter key calls the mock API function + with the current searchString
+
+
+test('Search Bar component', () => {
+
+  const mockSetSearchString = jest.fn()
+  const mockHandleSearch = jest.fn()
+  const initialSearchString = ''
+
+  const { searchBarComponent } = render(<SearchBar searchString={initialSearchString} setSearchString={mockSetSearchString} handleSearch={mockHandleSearch}/>)
+
+  //Test that the component renders properly and matches snapshot
+  it('should render properly and match snapshot', () => {
+    expect(searchBarComponent).toMatchSnapshot()
+  })
+})
+
+//Test to check that mockSetSearchString (i.e. mockFunction) is being invoked with the input value, as user is typing
+
+//Test that pressing the enter key calls the mockHandleSearch (i.e. mockFunction) + with the current searchString (mockSearchString)?
